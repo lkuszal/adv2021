283adde8136a99570cd85867851ac54ec52f75a0
lcholodecki <lcholodecki@northpass.com>
2021-12-02 09:49:07 +0100

    day 2

---
+++ day_02.rb
@@ -0,0 +1,61 @@
+class ShipNavigation
+  def initialize(horizontal=0, depth=0)
+    @horizontal = horizontal
+    @depth = depth
+  end
+  
+  def move(direction, distance)
+    case direction
+    when 'forward'
+      @horizontal += distance.to_i
+    when 'up'
+      @depth -= distance.to_i
+    when 'down'
+      @depth += distance.to_i
+    end
+  end
+
+  def end
+    return @horizontal, @depth
+  end
+end
+first_journey = ShipNavigation.new
+
+input = File.open('input2.txt')
+input_list = input.readlines.map(&:chomp)
+input_list.each do |instruction|
+  first_journey.move(*instruction.split)
+end
+x,y = first_journey.end
+puts x*y
+
+class ShipNavigation2
+  def initialize(horizontal=0, depth=0, aim=0)
+    @horizontal = horizontal
+    @depth = depth
+    @aim = aim
+  end
+
+  def move(direction, distance)
+    case direction
+    when 'forward'
+      @horizontal += distance.to_i
+      @depth += distance.to_i * @aim
+    when 'up'
+      @aim -= distance.to_i
+    when 'down'
+      @aim += distance.to_i
+    end
+  end
+
+  def end
+    return @horizontal, @depth
+  end
+end
+
+second_journey = ShipNavigation2.new
+input_list.each do |instruction|
+  second_journey.move(*instruction.split)
+end
+x,y = second_journey.end
+puts x*y
