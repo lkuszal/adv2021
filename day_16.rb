51b22062106dd931cc29999e285cdf49ca55f3fc
lcholodecki <lcholodecki@northpass.com>
2021-12-17 10:08:07 +0100

    now it's working!

--- day_16.rb
+++ day_16.rb
@@ -1,52 +1,41 @@
 input = File.open 'input16.txt'
 input_text = input.read.split("")
-binary_string = []
+$binary_string = []
 input_text.each do |hex_char|
-  binary_string.push(*('0000' + hex_char.to_i(16).to_s(2))[-4..].split(""))
+  $binary_string.push(*('0000' + hex_char.to_i(16).to_s(2))[-4..].split(""))
 end
 $sum_of_versions = 0
-def process_package(binary_string)
-  p binary_string.join
-  version = binary_string[..2].join.to_i(2)
+def process_package
+  version = $binary_string.shift(3).join.to_i(2)
   $sum_of_versions += version
-  type_id = binary_string[3..5].join.to_i(2)
-  binary_string.shift(6)
+  type_id = $binary_string.shift(3).join.to_i(2)
   if type_id == 4
-    p 4
     binary_value = ''
     last_bit = false
     until last_bit
-      if binary_string[0] == "0"
+      if $binary_string.shift == "0"
         last_bit = true
       end
-      binary_string.shift
-      binary_value += binary_string[...4].join
-      binary_string.shift(4)
+      binary_value += $binary_string.shift(4).join
     end
-
+    p binary_value.to_i(2)
   else
-    if binary_string[0] == "0"
-      binary_string.shift
-      p 0
-      total_length_of_subpackets = binary_string[...15].join.to_i(2)
-      binary_string.shift(15)
-      prime_length = binary_string.length
-      while prime_length - total_length_of_subpackets > binary_string.length
-        process_package(binary_string)
+    if $binary_string.shift == "0"
+      total_length_of_subpackets = $binary_string.shift(15).join.to_i(2)
+      prime_length = $binary_string.length
+      while prime_length - $binary_string.length < total_length_of_subpackets
+        process_package
       end
     else
-      binary_string.shift
-      p 1
-      number_of_contained_subpackets = binary_string[...11].join.to_i(2)
-      binary_string.shift(11)
+      number_of_contained_subpackets = $binary_string.shift(11).join.to_i(2)
       processed_subpackets = 0
       while number_of_contained_subpackets > processed_subpackets
-        process_package(binary_string)
+        process_package
         processed_subpackets += 1
       end
     end
   end
 end
 
-process_package(binary_string)
+process_package
 p $sum_of_versions
